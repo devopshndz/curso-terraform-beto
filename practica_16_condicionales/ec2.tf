fa62624f63e84a9d7ae7079b95c7fbcebd0d7bd2
Alberto Hernandez <devopsahernndez@gmail.com>
2023-08-20 14:48:16 -0500

    add practica_16

---
+++ practica_16_condicionales/ec2.tf
@@ -0,0 +1,28 @@
+# ec2 multiple con for each
+resource "aws_instance" "public_instance" {
+  for_each               = var.instancias
+  ami                    = var.ec2_specs.ami
+  instance_type          = var.ec2_specs.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  key_name               = data.aws_key_pair.key.key_name
+  vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
+
+  tags = {
+    Name = each.value 
+  }
+}
+
+# Desplegar una instancia a partir de una variable utilizando condicionales.
+resource "aws_instance" "monitoring_instance" {
+  count = var.enable_monitoring ? 1 : 0 
+  #count = var.enable_monitoring == 1 ? 1 : 0
+  ami                    = var.ec2_specs.ami
+  instance_type          = var.ec2_specs.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  key_name               = data.aws_key_pair.key.key_name
+  vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
+
+  tags = {
+    Name = "Monitoreo"
+  }
+}
